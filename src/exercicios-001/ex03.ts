05846a636c35862eec8d409dc826710f6fca664b
layzamedeiros <165849168+layzamedeiros@users.noreply.github.com>
2025-06-22 15:18:42 -0300

    Exercício 03 finalizado.

---
+++ src/exercicios-001/ex03.ts
@@ -0,0 +1,24 @@
+let numero1: number = 8;
+let numero2: number = 4;
+
+let soma: number = numero1 + numero2;
+let subtracao: number = numero1 - numero2;
+
+console.log(soma, subtracao);
+
+let primeiroNome: string = "Layza";
+let segundoNome: string = "Medeiros";
+
+let somaNome: string = primeiroNome + segundoNome;
+
+console.log(somaNome);
+
+let estaAtiva: boolean = true;
+
+if (estaAtiva) {
+  console.log("Está ativa!")
+};
+
+let valorIndefinido: undefined = undefined;
+
+console.log(valorIndefinido);
