ea6024bf8aed7fc97e248e155f2045e64ced6842
layzamedeiros <165849168+layzamedeiros@users.noreply.github.com>
2025-06-22 14:59:19 -0300

    Exercício 02 finalizado.

---
+++ src/exercicios-001/ex02.ts
@@ -0,0 +1,11 @@
+let usuario: string = "Layza"; 
+// Escopo de bloco: respeita blocos como if ou for.
+// Mutável: pode ser reatribuída.
+
+const matricula: number = 201514; 
+// Escopo de bloco: respeita blocos como if ou for.
+// Imutável: não pode ser reatribuída.
+
+var empresa: string = "TechInside"; 
+// Escopo de função: não respeita blocos como if ou for.
+// Mutável: pode ser reatribuída.
