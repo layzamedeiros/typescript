c2bd2001f61a8157dee28e0e63ccae798ab1c612
layzamedeiros <165849168+layzamedeiros@users.noreply.github.com>
2025-06-22 15:28:20 -0300

    Exercício 05 finalizado.

---
+++ src/exercicios-001/ex05.ts
@@ -0,0 +1,3 @@
+let a = "Layza"; 
+// a = 2;
+// Ao tentar atribuir um número a variável ele mostra o erro: "O tipo 'number' não pode ser atribuído ao tipo 'string'." O TypeScript inferiu que a variável é do tipo string.
